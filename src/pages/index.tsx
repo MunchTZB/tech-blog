7003b742158fabab15f7062c03c53b6a1410d96a
Munch_TZB <dev.tzb@gmail.com>
2020-11-04 14:17:43 +0800

    update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,28 @@
 import React from 'react'
 import Layout from '../components/layout';
+import { Link } from 'gatsby'
 
 export default () => {
   return (
     <Layout>
-      <div className="g-p5r home">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus quam maxime ducimus enim minus molestias, natus eius fugit culpa quae voluptatem odio illo quis? Molestias, praesentium commodi? Pariatur, provident molestias.
-      </div>
+      <ul className="blogs">
+        <Link to="/about">
+          <li className="blogs__item">
+            <div className="left">
+              <div className="left__time">
+                2020年 11月 4日
+              </div>
+              <div className="left__title">
+                怎么训练一个打砖块机器人 - Chapter 1
+              </div>
+              <div className="left__desc">
+                这一章中，我们完成了打砖块机器人的前置需求，先完成一个打砖块游戏。
+              </div>
+            </div>
+            <div className="right"></div>
+          </li>
+        </Link>
+      </ul>
     </Layout>
   )
 }
